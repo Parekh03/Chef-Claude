d5e19575807d273021ecf40a1d87c32d6d0079c3
PAREKH VED <110220047+Parekh03@users.noreply.github.com>
2025-02-19 19:54:58 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return (
         <div className="header-div">
-            <img src="https://imgur.com/Hr9UtNo" alt="Chef Claude Icon" className="header-icon" />
+            <img src="https://i.imgur.com/Hr9UtNo.png" alt="Chef Claude Icon" className="header-icon" />
             <h1 className="header-heading">Chef Claude</h1>
         </div>
     )
